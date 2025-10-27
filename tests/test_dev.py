732803c87a85d940e5936274842da59c05a4dfa3
jmaczan <jedrzejpawel@maczan.pl>
2025-10-27 14:48:13 +0100

    Register webgpu as privateuse1 backend

---
+++ tests/test_dev.py
@@ -0,0 +1,19 @@
+import torch
+import torch_webgpu
+
+
+def main():
+    print(torch._C._get_privateuse1_backend_name())
+
+    dev = torch.device("webgpu")
+    print(dev)
+
+    dev = torch.device("webgpu:0")
+    print(dev)
+
+    empty = torch.empty((4, 4), device="webgpu")
+    print(empty)
+
+
+if __name__ == "__main__":
+    main()
