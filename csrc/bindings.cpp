4d5b8b7ea928671e4a04c18ea65d3b55deac4086
jmaczan <jedrzejpawel@maczan.pl>
2025-11-03 17:48:44 +0100

    A very basic device guard for webgpu. Breaks on runtime yet

--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -27,6 +27,61 @@ static void WebGPUCachingHostDeleter(void *ptr)
     free(ptr);
 }
 
+static thread_local int current_webgpu_device = 0;
+static int webgpu_device_count = 1;
+static int webgpu_stream_count = 1;
+
+struct WebGPUGuardImpl final : public c10::impl::DeviceGuardImplInterface
+{
+    static constexpr c10::DeviceType static_type = c10::DeviceType::PrivateUse1;
+
+    WebGPUGuardImpl() {}
+
+    c10::DeviceType type() const
+    {
+        return c10::DeviceType::PrivateUse1;
+    }
+
+    c10::Device exchangeDevice(c10::Device d) const
+    {
+        c10::Device old_device = getDevice();
+        setDevice(d);
+        return old_device;
+    }
+
+    c10::Device getDevice() const
+    {
+        return c10::Device(c10::DeviceType::PrivateUse1, current_webgpu_device);
+    }
+
+    void setDevice(c10::Device d) const
+    {
+        current_webgpu_device = d.index();
+    }
+
+    void uncheckedSetDevice(c10::Device d) const noexcept
+    {
+        setDevice(d);
+    }
+
+    c10::DeviceIndex deviceCount() const noexcept
+    {
+        return webgpu_device_count;
+    }
+
+    c10::Stream getStream(c10::Device d) const noexcept
+    {
+        return c10::Stream(c10::Stream::DEFAULT, getDevice());
+    }
+
+    c10::Stream exchangeStream(c10::Stream stream) const noexcept
+    {
+        return getStream(getDevice());
+    }
+};
+
+C10_REGISTER_GUARD_IMPL(PrivateUse1, WebGPUGuardImpl);
+
 // not really caching, yet
 struct WebGPUCachingAllocator final : public c10::Allocator
 {
--- tests/test_dev.py
+++ tests/test_dev.py
@@ -3,21 +3,10 @@ import torch_webgpu
 
 
 def main():
-    print(torch._C._get_privateuse1_backend_name())
-
-    dev = torch.device("webgpu")
-    print(dev)
-
-    dev = torch.device("webgpu:0")
-    print(dev)
-
-    empty = torch.empty((4, 4), device="webgpu")
-    print(empty.device, empty.shape)
-    empty_cpu = empty.to("cpu")
-    print(empty_cpu.device, empty_cpu.shape)
-    # print(torch._C._dispatch_dump_table("aten::view"))
-
-    # print(empty)
+    cpu = torch.empty((4, 4), dtype=torch.float32)
+    wgpu = cpu.to("webgpu")
+    back = cpu.to("cpu")
+    print(torch.allclose(cpu, back))
 
 
 if __name__ == "__main__":
