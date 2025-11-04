b7c5fa0aa7fe60663109d9a152ef3462d02887f9
jmaczan <jedrzejpawel@maczan.pl>
2025-11-04 20:26:43 +0100

    Set up a singleton of webgpu instance, adapter and device (a single one)

--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -9,6 +9,64 @@
 #include <cstdlib>
 #include <webgpu/webgpu.h>
 #include <webgpu/webgpu_cpp.h>
+#include <iostream>
+
+struct WebGPUContext
+{
+    wgpu::Instance instance;
+    wgpu::Adapter adapter;
+    wgpu::Device device;
+
+    WebGPUContext()
+    {
+        static const auto k_timed_wait_any = wgpu::InstanceFeatureName::TimedWaitAny;
+        wgpu::InstanceDescriptor instance_descriptor{
+            .requiredFeatureCount = 1,
+            .requiredFeatures = &k_timed_wait_any};
+        instance = wgpu::CreateInstance(&instance_descriptor);
+
+        wgpu::Future adapter_future = instance.RequestAdapter(
+            nullptr, wgpu::CallbackMode::WaitAnyOnly,
+            [this](wgpu::RequestAdapterStatus status, wgpu::Adapter a, wgpu::StringView message)
+            {
+                if (status != wgpu::RequestAdapterStatus::Success)
+                {
+                    std::cout << "Failed to load WebGPU Adapter" << "\n";
+                    exit(1);
+                }
+                this->adapter = std::move(a);
+            });
+        instance.WaitAny(adapter_future, UINT64_MAX);
+
+        wgpu::DeviceDescriptor device_descriptor;
+        device_descriptor.SetUncapturedErrorCallback([](const wgpu::Device &, wgpu::ErrorType errorType, wgpu::StringView message)
+                                                     { std::cout << "Error in device descriptor" << "\n"; });
+
+        wgpu::Future device_future = adapter.RequestDevice(
+            &device_descriptor, wgpu::CallbackMode::WaitAnyOnly,
+            [this](wgpu::RequestDeviceStatus status, wgpu::Device d, wgpu::StringView message)
+            {
+                if (status != wgpu::RequestDeviceStatus::Success)
+                {
+                    std::cout << "Request WebGPU device failed" << "\n";
+                    exit(1);
+                }
+                this->device = std::move(d);
+            });
+        instance.WaitAny(device_future, UINT64_MAX);
+    }
+
+    wgpu::Device getDevice()
+    {
+        return device;
+    }
+};
+
+static WebGPUContext &getWebGPUContext()
+{
+    static WebGPUContext webgpu_context;
+    return webgpu_context;
+}
 
 struct WebGPUAllocator
 {
