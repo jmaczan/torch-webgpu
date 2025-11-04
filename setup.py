3002c689dd671f0d9dd62ff4abe7edd9f44d28e5
jmaczan <jedrzejpawel@maczan.pl>
2025-11-04 15:12:15 +0100

    Search for all .cpp files to be included from csrc

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from torch.utils.cpp_extension import (
     BuildExtension,
     CppExtension,
 )
+import glob
 
 setup(
     name="torch-webgpu",
     description="WebGPU for PyTorch",
     ext_modules=[
-        CppExtension("torch_webgpu._C", sources=["csrc/bindings.cpp"]),
+        CppExtension("torch_webgpu._C", sources=glob.glob("csrc/*.cpp")),
     ],
     cmdclass={"build_ext": BuildExtension},
     package_dir={"": "python"},
