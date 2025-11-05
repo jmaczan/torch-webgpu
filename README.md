099ee87ded636a40cc085bb4e2effddbfa6a4ccf
jmaczan <jedrzejpawel@maczan.pl>
2025-11-05 14:01:10 +0100

    copy_ for cpu->webgpu and webgpu->webgpu

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # torch-webgpu
 WebGPU backend for PyTorch
 
-## Install `google/dawn`
+## Install
+1. Clone this repo
+
+2. Install `google/dawn`
 Guide: https://github.com/google/dawn/blob/main/docs/quickstart-cmake.md. Set `DAWN_PREFIX=` to `dawn/install/Release` based on there is your `dawn` repo, like `DAWN_PREFIX=/home/user/dawn/install/Release`
+
+3. In this repo, run `./build.sh`
+
+## Use
+`import torch_webgpu`
+
+And now you can use `device="webgpu"` in pytorch!
 
 
 Note: This project is unrelated to [webgpu-torch](https://github.com/praeclarum/webgpu-torch), which is a neat PyTorch reimplementation in TypeScript targeting WebGPU
+
+I mainly use Ascend's NPU backend for PyTorch https://github.com/ascend/pytorch, Elie's WebGPU guide https://eliemichel.github.io/LearnWebGPU/index.html and PyTorch PrivateUse1 custom backend docs as a reference https://docs.pytorch.org/tutorials/advanced/privateuseone.html https://docs.pytorch.org/tutorials/advanced/extend_dispatcher.html https://docs.pytorch.org/tutorials/advanced/dispatcher
--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -16,6 +16,7 @@ struct WebGPUContext
     wgpu::Instance instance;
     wgpu::Adapter adapter;
     wgpu::Device device;
+    wgpu::Queue queue;
 
     WebGPUContext()
     {
@@ -52,6 +53,7 @@ struct WebGPUContext
                     exit(1);
                 }
                 this->device = std::move(d);
+                this->queue = device.GetQueue();
             });
         instance.WaitAny(device_future, UINT64_MAX);
     }
@@ -59,6 +61,11 @@ struct WebGPUContext
     wgpu::Device getDevice()
     {
         return device;
+    }
+
+    wgpu::Queue getQueue()
+    {
+        return queue;
     }
 };
 
@@ -212,8 +219,48 @@ at::Tensor empty_strided(
 at::Tensor &copy_(
     at::Tensor &self, at::Tensor const &src, bool non_blocking = false)
 {
-    auto size = self.numel() * at::elementSize(self.dtype().toScalarType());
-    std::memcpy(self.data_ptr(), src.data_ptr(), size);
+    // TODO: take non_blocking into consideration
+
+    TORCH_CHECK(src.is_contiguous());
+    TORCH_CHECK(self.is_contiguous());
+
+    auto src_size = src.numel() * at::elementSize(src.dtype().toScalarType()); // TODO: probably some size check here?
+    auto self_size = self.numel() * at::elementSize(self.dtype().toScalarType());
+    TORCH_CHECK(self_size == src_size);
+
+    if (src.device().is_cpu() && self.device().is_privateuseone())
+    {
+        TORCH_CHECK(src.dtype() == self.dtype());
+        TORCH_CHECK(src.numel() == self.numel());
+        auto src_data = src.data_ptr();
+        auto self_data = static_cast<WebGPUAllocation *>(self.data_ptr());
+
+        TORCH_CHECK(self_data->buffer.GetSize() >= src_size);
+
+        getWebGPUContext().getQueue().WriteBuffer(self_data->buffer, 0, src_data, src_size);
+    }
+    else if (src.device().is_privateuseone() && self.device().is_privateuseone())
+    {
+        TORCH_CHECK(src.dtype() == self.dtype());
+        TORCH_CHECK(src.numel() == self.numel());
+
+        auto src_data = static_cast<WebGPUAllocation *>(src.data_ptr());
+        auto self_data = static_cast<WebGPUAllocation *>(self.data_ptr());
+
+        TORCH_CHECK(src_data->buffer.GetSize() >= src_size);
+        TORCH_CHECK(self_data->buffer.GetSize() >= src_size);
+
+        wgpu::CommandEncoder encoder = getWebGPUContext().getDevice().CreateCommandEncoder();
+        encoder.CopyBufferToBuffer(src_data->buffer, 0, self_data->buffer, 0, src_size);
+        wgpu::CommandBuffer command = encoder.Finish();
+
+        getWebGPUContext().getQueue().Submit(1, &command); // TODO: Submit is async, handle it correctly
+    }
+    else
+    {
+        TORCH_CHECK(false, "copy_ to WebGPU is not supported for this source device");
+    }
+
     return self;
 }
 
