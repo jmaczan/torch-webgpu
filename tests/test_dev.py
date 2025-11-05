8fc04f22382c6cfac42bfc810e538e4f3eeb1dd2
jmaczan <jedrzejpawel@maczan.pl>
2025-11-05 20:46:46 +0100

    Copy from webgpu to CPU

--- tests/test_dev.py
+++ tests/test_dev.py
@@ -5,6 +5,7 @@ import torch_webgpu
 def main():
     wgpu = torch.empty((4, 4), device="webgpu", dtype=torch.float32)
     wgpu2 = wgpu.to("webgpu")
+    cpu = wgpu2.to("cpu")
     print(torch.allclose(wgpu, wgpu2))
 
 
