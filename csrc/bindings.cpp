0225e5d8f57c4c4bfceb7d1f05001bfa8921d019
jmaczan <jedrzejpawel@maczan.pl>
2025-11-09 23:57:58 +0100

    Turn add.Tensor into TensorInterator-based add_stub impl

--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -1,5 +1,8 @@
 #include <ATen/ATen.h>
 #include <ATen/RedispatchFunctions.h>
+#include <ATen/TensorIterator.h>
+#include <ATen/native/DispatchStub.h>
+#include <ATen/native/BinaryOps.h>
 #include <torch/library.h>
 #include <ATen/EmptyTensor.h>
 #include <ATen/native/CPUFallback.h>
@@ -351,172 +354,6 @@ at::Tensor _copy_from(at::Tensor const &self, at::Tensor const &dst, bool non_bl
     return dst;
 }
 
-at::Tensor add(at::Tensor const &self, at::Tensor const &other, at::Scalar const &alpha = 1)
-{
-    WebGPUContext &ctx = getWebGPUContext();
-    constexpr const char *addWGSL = R"wgsl(
-    struct Params {
-        length: u32,
-        alpha: f32,
-    };
-
-    @group(0) @binding(0)
-    var<storage, read> selfBuffer: array<f32>;
-
-    @group(0) @binding(1)
-    var<storage, read> otherBuffer: array<f32>;
-
-    @group(0) @binding(2)
-    var<storage, read_write> outBuffer: array<f32>;
-
-    @group(0) @binding(3)
-    var<uniform> params: Params;
-
-    @compute @workgroup_size(64)
-    fn main(@builtin(global_invocation_id) gid: vec3<u32>) {
-        let i = gid.x;
-        if (i >= params.length) { return; }
-        outBuffer[i] = selfBuffer[i] + params.alpha * otherBuffer[i];
-    }
-    )wgsl";
-
-    wgpu::ShaderSourceWGSL shader_source{
-        wgpu::ShaderSourceWGSL::Init{
-            nullptr,
-            wgpu::StringView{addWGSL, std::strlen(addWGSL)},
-        }};
-
-    wgpu::ShaderModuleDescriptor shader_descriptor{};
-    shader_descriptor.nextInChain = &shader_source;
-    shader_descriptor.label = "at::Tensor add shader";
-    wgpu::ShaderModule shader_module = ctx.getDevice().CreateShaderModule(&shader_descriptor);
-
-    wgpu::BindGroupLayoutEntry bindings[4]{};
-
-    bindings[0].binding = 0;
-    bindings[0].visibility = wgpu::ShaderStage::Compute;
-    bindings[0].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
-    bindings[0].buffer.hasDynamicOffset = false;
-    bindings[0].buffer.minBindingSize = 0;
-
-    bindings[1].binding = 1;
-    bindings[1].visibility = wgpu::ShaderStage::Compute;
-    bindings[1].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
-    bindings[1].buffer.hasDynamicOffset = false;
-    bindings[1].buffer.minBindingSize = 0;
-
-    bindings[2].binding = 2;
-    bindings[2].visibility = wgpu::ShaderStage::Compute;
-    bindings[2].buffer.type = wgpu::BufferBindingType::Storage;
-    bindings[2].buffer.hasDynamicOffset = false;
-    bindings[2].buffer.minBindingSize = 0;
-
-    bindings[3].binding = 3;
-    bindings[3].visibility = wgpu::ShaderStage::Compute;
-    bindings[3].buffer.type = wgpu::BufferBindingType::Uniform;
-    bindings[3].buffer.hasDynamicOffset = false;
-    bindings[3].buffer.minBindingSize = 0;
-
-    wgpu::BindGroupLayoutDescriptor layout_descriptor{};
-    layout_descriptor.entryCount = 4;
-    layout_descriptor.entries = bindings;
-
-    wgpu::BindGroupLayout bind_group_layout = ctx.getDevice().CreateBindGroupLayout(&layout_descriptor);
-
-    wgpu::PipelineLayoutDescriptor pipeline_layout_descriptor{};
-    pipeline_layout_descriptor.bindGroupLayoutCount = 1;
-    pipeline_layout_descriptor.bindGroupLayouts = &bind_group_layout;
-
-    wgpu::PipelineLayout pipeline_layout = ctx.getDevice().CreatePipelineLayout(&pipeline_layout_descriptor);
-
-    wgpu::ComputePipelineDescriptor pipeline_descriptor{};
-    pipeline_descriptor.layout = pipeline_layout;
-    pipeline_descriptor.compute.module = shader_module;
-    pipeline_descriptor.compute.entryPoint = wgpu::StringView{"main", 4};
-
-    wgpu::ComputePipeline pipeline = ctx.getDevice().CreateComputePipeline(&pipeline_descriptor);
-
-    // everything above should be cached, uniform (params) perhaps too?
-    TORCH_CHECK(self.dtype() == other.dtype());
-    TORCH_CHECK(self.is_contiguous());
-    TORCH_CHECK(other.is_contiguous());
-    TORCH_CHECK(self.sizes().equals(other.sizes()));
-
-    at::Tensor out = at::empty_like(self, self.options().device(self.device()));
-
-    WebGPUAllocation *self_allocation = static_cast<WebGPUAllocation *>(self.data_ptr());
-    WebGPUAllocation *other_allocation = static_cast<WebGPUAllocation *>(other.data_ptr());
-    WebGPUAllocation *out_allocation = static_cast<WebGPUAllocation *>(out.data_ptr());
-
-    wgpu::Buffer self_buffer = self_allocation->buffer;
-    wgpu::Buffer other_buffer = other_allocation->buffer;
-    wgpu::Buffer out_buffer = out_allocation->buffer;
-
-    uint32_t numel = static_cast<uint32_t>(self.numel());
-    size_t bytes = numel * at::elementSize(self.scalar_type());
-
-    struct Params
-    {
-        uint32_t length;
-        float alpha;
-    };
-    Params params{numel, alpha.to<float>()};
-
-    wgpu::BufferDescriptor uniform_descriptor{};
-    uniform_descriptor.label = "Params";
-    uniform_descriptor.usage = wgpu::BufferUsage::Uniform | wgpu::BufferUsage::CopyDst;
-    uniform_descriptor.size = sizeof(Params);
-    uniform_descriptor.mappedAtCreation = false;
-
-    wgpu::Buffer params_buffer = ctx.getDevice().CreateBuffer(&uniform_descriptor);
-    ctx.getQueue().WriteBuffer(params_buffer, 0, &params, sizeof(Params));
-
-    wgpu::BindGroupEntry bind_group_entries[4]{};
-    bind_group_entries[0].binding = 0;
-    bind_group_entries[0].buffer = self_buffer;
-    bind_group_entries[0].offset = 0;
-    bind_group_entries[0].size = bytes;
-
-    bind_group_entries[1].binding = 1;
-    bind_group_entries[1].buffer = other_buffer;
-    bind_group_entries[1].offset = 0;
-    bind_group_entries[1].size = bytes;
-
-    bind_group_entries[2].binding = 2;
-    bind_group_entries[2].buffer = out_buffer;
-    bind_group_entries[2].offset = 0;
-    bind_group_entries[2].size = bytes;
-
-    bind_group_entries[3].binding = 3;
-    bind_group_entries[3].buffer = params_buffer;
-    bind_group_entries[3].offset = 0;
-    bind_group_entries[3].size = sizeof(Params);
-
-    wgpu::BindGroupDescriptor bind_group_descriptor{};
-    bind_group_descriptor.layout = bind_group_layout;
-    bind_group_descriptor.entryCount = 4;
-    bind_group_descriptor.entries = bind_group_entries;
-
-    wgpu::BindGroup bind_group = ctx.getDevice().CreateBindGroup(&bind_group_descriptor);
-
-    wgpu::CommandEncoder encoder = ctx.getDevice().CreateCommandEncoder();
-    wgpu::ComputePassDescriptor pass_descriptor;
-    wgpu::ComputePassEncoder pass_encoder = encoder.BeginComputePass(&pass_descriptor);
-    pass_encoder.SetPipeline(pipeline);
-    pass_encoder.SetBindGroup(0, bind_group);
-
-    const uint32_t workgroup_size = 64;
-    uint32_t num_workgroups = (numel + workgroup_size - 1) / workgroup_size;
-
-    pass_encoder.DispatchWorkgroups(num_workgroups);
-    pass_encoder.End();
-
-    wgpu::CommandBuffer command_buffer = encoder.Finish();
-    ctx.getQueue().Submit(1, &command_buffer);
-
-    return out;
-}
-
 at::Tensor view(at::Tensor const &self, at::IntArrayRef size)
 {
     return at::native::view(self, size);
@@ -540,6 +377,297 @@ static void webgpu_cpu_fallback_boxed(const c10::OperatorHandle &op, torch::jit:
 {
     at::native::cpu_fallback(op, stack);
 }
+using at::native::add_stub;
+
+namespace at
+{
+    namespace native
+    {
+
+        void add_kernel_webgpu(TensorIteratorBase &iter,
+                               const Scalar &alpha)
+        {
+            TORCH_CHECK(iter.ntensors() == 3);
+            TORCH_CHECK(iter.common_dtype() == at::ScalarType::Float);
+            TORCH_CHECK(iter.device_type() == c10::DeviceType::PrivateUse1);
+            TORCH_CHECK(iter.dtype(0) == iter.dtype(1));
+            TORCH_CHECK(iter.dtype(1) == iter.dtype(2));
+
+            WebGPUContext &ctx = getWebGPUContext();
+            constexpr const char *addWGSL = R"wgsl(
+const MAX_DIMS: u32 = 8u;
+
+struct Params {
+    length: u32,
+    ndim: u32,
+    alpha: f32,
+    out_strides: array<u32, MAX_DIMS>,
+    self_strides: array<u32, MAX_DIMS>,
+    other_strides: array<u32, MAX_DIMS>,
+    shape: array<u32, MAX_DIMS>,
+};
+
+@group(0) @binding(0)
+var<storage, read> selfBuffer: array<f32>;
+
+@group(0) @binding(1)
+var<storage, read> otherBuffer: array<f32>;
+
+@group(0) @binding(2)
+var<storage, read_write> outBuffer: array<f32>;
+
+@group(0) @binding(3)
+var<uniform> params: Params;
+
+@compute @workgroup_size(64)
+fn main(@builtin(global_invocation_id) gid: vec3<u32>) {
+    let i = gid.x;
+    if (i >= params.length) { return; }
+
+    var remaining = i;
+    var coord: array<u32, MAX_DIMS>;
+
+    for (var d: i32 = i32(params.ndim) - 1; d >= 0; d--) {
+        let ud = u32(d);
+        let s = params.shape[ud];
+        coord[ud] = remaining % s;
+        remaining = remaining / s;
+    }
+
+    var idx_out: u32 = 0;
+    var idx_self: u32 = 0;
+    var idx_other: u32 = 0;
+
+    for (var d: u32 = 0u; d < params.ndim; d++) {
+        let c = coord[d];
+        idx_out += c * params.out_strides[d];
+        idx_self += c * params.self_strides[d];
+        idx_other += c * params.other_strides[d];
+    }
+
+    outBuffer[idx_out] = selfBuffer[idx_self] + params.alpha * otherBuffer[idx_other];
+}
+)wgsl";
+
+            wgpu::ShaderSourceWGSL shader_source{
+                wgpu::ShaderSourceWGSL::Init{
+                    nullptr,
+                    wgpu::StringView{addWGSL, std::strlen(addWGSL)},
+                }};
+
+            wgpu::ShaderModuleDescriptor shader_descriptor{};
+            shader_descriptor.nextInChain = &shader_source;
+            shader_descriptor.label = "at::Tensor add shader";
+            wgpu::ShaderModule shader_module = ctx.getDevice().CreateShaderModule(&shader_descriptor);
+
+            wgpu::BindGroupLayoutEntry bindings[4]{};
+
+            bindings[0].binding = 0;
+            bindings[0].visibility = wgpu::ShaderStage::Compute;
+            bindings[0].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
+            bindings[0].buffer.hasDynamicOffset = false;
+            bindings[0].buffer.minBindingSize = 0;
+
+            bindings[1].binding = 1;
+            bindings[1].visibility = wgpu::ShaderStage::Compute;
+            bindings[1].buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
+            bindings[1].buffer.hasDynamicOffset = false;
+            bindings[1].buffer.minBindingSize = 0;
+
+            bindings[2].binding = 2;
+            bindings[2].visibility = wgpu::ShaderStage::Compute;
+            bindings[2].buffer.type = wgpu::BufferBindingType::Storage;
+            bindings[2].buffer.hasDynamicOffset = false;
+            bindings[2].buffer.minBindingSize = 0;
+
+            bindings[3].binding = 3;
+            bindings[3].visibility = wgpu::ShaderStage::Compute;
+            bindings[3].buffer.type = wgpu::BufferBindingType::Uniform;
+            bindings[3].buffer.hasDynamicOffset = false;
+            bindings[3].buffer.minBindingSize = 0;
+
+            wgpu::BindGroupLayoutDescriptor layout_descriptor{};
+            layout_descriptor.entryCount = 4;
+            layout_descriptor.entries = bindings;
+
+            wgpu::BindGroupLayout bind_group_layout = ctx.getDevice().CreateBindGroupLayout(&layout_descriptor);
+
+            wgpu::PipelineLayoutDescriptor pipeline_layout_descriptor{};
+            pipeline_layout_descriptor.bindGroupLayoutCount = 1;
+            pipeline_layout_descriptor.bindGroupLayouts = &bind_group_layout;
+
+            wgpu::PipelineLayout pipeline_layout = ctx.getDevice().CreatePipelineLayout(&pipeline_layout_descriptor);
+
+            wgpu::ComputePipelineDescriptor pipeline_descriptor{};
+            pipeline_descriptor.layout = pipeline_layout;
+            pipeline_descriptor.compute.module = shader_module;
+            pipeline_descriptor.compute.entryPoint = wgpu::StringView{"main", 4};
+
+            wgpu::ComputePipeline pipeline = ctx.getDevice().CreateComputePipeline(&pipeline_descriptor);
+            // everything above should be cached, uniform (params) perhaps too?
+            auto out = iter.tensor(0);
+            auto self = iter.tensor(1);
+            auto other = iter.tensor(2);
+            auto ndim = iter.ndim();
+            auto shape = iter.shape();
+            auto out_strides_bytes = iter.strides(0);
+            auto self_strides_bytes = iter.strides(1);
+            auto other_strides_bytes = iter.strides(2);
+
+            auto element_size = iter.element_size(0);
+
+            std::vector<int64_t> out_strides(ndim);
+            std::vector<int64_t> self_strides(ndim);
+            std::vector<int64_t> other_strides(ndim);
+
+            for (int64_t i = 0; i < ndim; ++i)
+            {
+                int64_t out_bytes = out_strides_bytes[i];
+                if (out_bytes == 0)
+                {
+                    out_strides[i] = 0;
+                }
+                else
+                {
+                    TORCH_CHECK(out_bytes % element_size == 0);
+                    out_strides[i] = out_bytes / element_size;
+                }
+
+                int64_t self_bytes = self_strides_bytes[i];
+                if (self_bytes == 0)
+                {
+                    self_strides[i] = 0;
+                }
+                else
+                {
+                    TORCH_CHECK(self_bytes % element_size == 0);
+                    self_strides[i] = self_bytes / element_size;
+                }
+
+                int64_t other_bytes = other_strides_bytes[i];
+                if (other_bytes == 0)
+                {
+                    other_strides[i] = 0;
+                }
+                else
+                {
+                    TORCH_CHECK(other_bytes % element_size == 0);
+                    other_strides[i] = other_bytes / element_size;
+                }
+            }
+
+            auto length = iter.numel();
+
+            WebGPUAllocation *out_allocation = static_cast<WebGPUAllocation *>(iter.data_ptr(0));
+            WebGPUAllocation *self_allocation = static_cast<WebGPUAllocation *>(iter.data_ptr(1));
+            WebGPUAllocation *other_allocation = static_cast<WebGPUAllocation *>(iter.data_ptr(2));
+
+            wgpu::Buffer self_buffer = self_allocation->buffer;
+            self_buffer.GetSize();
+            wgpu::Buffer other_buffer = other_allocation->buffer;
+            wgpu::Buffer out_buffer = out_allocation->buffer;
+
+            constexpr uint32_t MAX_DIMS = 8;
+
+            struct Params
+            {
+                uint32_t length;
+                float alpha;
+                uint32_t ndim;
+                uint32_t out_strides[MAX_DIMS];
+                uint32_t self_strides[MAX_DIMS];
+                uint32_t other_strides[MAX_DIMS];
+                uint32_t shape[MAX_DIMS];
+            };
+
+            Params params{};
+            params.length = static_cast<uint32_t>(iter.numel());
+            params.alpha = alpha.to<float>();
+            params.ndim = static_cast<uint32_t>(ndim);
+
+            for (uint32_t d = 0; d < MAX_DIMS; ++d)
+            {
+                params.out_strides[d] = 0;
+                params.self_strides[d] = 0;
+                params.other_strides[d] = 0;
+                params.shape[d] = 1;
+            }
+
+            for (int64_t i = 0; i < ndim; ++i)
+            {
+                auto dim_size = shape[i];
+                TORCH_CHECK(dim_size >= 0 && dim_size <= std::numeric_limits<uint32_t>::max());
+                params.shape[i] = static_cast<uint32_t>(dim_size);
+
+                auto out_stride = out_strides[i];
+                auto self_stride = self_strides[i];
+                auto other_stride = other_strides[i];
+
+                TORCH_CHECK(out_stride >= 0 && out_stride <= std::numeric_limits<uint32_t>::max());
+                TORCH_CHECK(self_stride >= 0 && self_stride <= std::numeric_limits<uint32_t>::max());
+                TORCH_CHECK(other_stride >= 0 && other_stride <= std::numeric_limits<uint32_t>::max());
+
+                params.out_strides[i] = static_cast<uint32_t>(out_stride);
+                params.self_strides[i] = static_cast<uint32_t>(self_stride);
+                params.other_strides[i] = static_cast<uint32_t>(other_stride);
+            }
+
+            wgpu::BufferDescriptor uniform_descriptor{};
+            uniform_descriptor.label = "Params";
+            uniform_descriptor.usage = wgpu::BufferUsage::Uniform | wgpu::BufferUsage::CopyDst;
+            uniform_descriptor.size = sizeof(Params);
+            uniform_descriptor.mappedAtCreation = false;
+
+            wgpu::Buffer params_buffer = ctx.getDevice().CreateBuffer(&uniform_descriptor);
+            ctx.getQueue().WriteBuffer(params_buffer, 0, &params, sizeof(Params));
+
+            wgpu::BindGroupEntry bind_group_entries[4]{};
+            bind_group_entries[0].binding = 0;
+            bind_group_entries[0].buffer = self_buffer;
+            bind_group_entries[0].offset = 0;
+            bind_group_entries[0].size = self_buffer.GetSize();
+
+            bind_group_entries[1].binding = 1;
+            bind_group_entries[1].buffer = other_buffer;
+            bind_group_entries[1].offset = 0;
+            bind_group_entries[1].size = other_buffer.GetSize();
+
+            bind_group_entries[2].binding = 2;
+            bind_group_entries[2].buffer = out_buffer;
+            bind_group_entries[2].offset = 0;
+            bind_group_entries[2].size = out_buffer.GetSize();
+
+            bind_group_entries[3].binding = 3;
+            bind_group_entries[3].buffer = params_buffer;
+            bind_group_entries[3].offset = 0;
+            bind_group_entries[3].size = sizeof(Params);
+
+            wgpu::BindGroupDescriptor bind_group_descriptor{};
+            bind_group_descriptor.layout = bind_group_layout;
+            bind_group_descriptor.entryCount = 4;
+            bind_group_descriptor.entries = bind_group_entries;
+
+            wgpu::BindGroup bind_group = ctx.getDevice().CreateBindGroup(&bind_group_descriptor);
+
+            wgpu::CommandEncoder encoder = ctx.getDevice().CreateCommandEncoder();
+            wgpu::ComputePassDescriptor pass_descriptor;
+            wgpu::ComputePassEncoder pass_encoder = encoder.BeginComputePass(&pass_descriptor);
+            pass_encoder.SetPipeline(pipeline);
+            pass_encoder.SetBindGroup(0, bind_group);
+
+            const uint32_t workgroup_size = 64;
+            uint32_t num_workgroups = (length + workgroup_size - 1) / workgroup_size;
+
+            pass_encoder.DispatchWorkgroups(num_workgroups);
+            pass_encoder.End();
+
+            wgpu::CommandBuffer command_buffer = encoder.Finish();
+            ctx.getQueue().Submit(1, &command_buffer);
+        }
+
+        REGISTER_PRIVATEUSE1_DISPATCH(add_stub, &add_kernel_webgpu);
+    }
+}
 
 TORCH_LIBRARY_IMPL(aten, PrivateUse1, m)
 {
@@ -547,7 +675,6 @@ TORCH_LIBRARY_IMPL(aten, PrivateUse1, m)
     m.impl("empty_strided", TORCH_FN(empty_strided));
     m.impl("copy_", TORCH_FN(copy_));
     m.impl("_copy_from", TORCH_FN(_copy_from));
-    m.impl("add.Tensor", TORCH_FN(add));
     m.impl("view", TORCH_FN(view));
     m.impl("resize_", TORCH_FN(resize_));
     m.impl("_copy_from_and_resize", TORCH_FN(_copy_from_and_resize));
