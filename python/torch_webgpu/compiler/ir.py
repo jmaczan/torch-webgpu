8e612e240e7804e5ce2b71dddf4399c8093ba02d
jmaczan <jedrzejpawel@maczan.pl>
2025-12-09 20:43:46 +0100

    Map test_dev fx nodes into a silly little IR

--- python/torch_webgpu/compiler/ir.py
+++ python/torch_webgpu/compiler/ir.py
@@ -1,28 +1,39 @@
 class IRNode:
-    def __init__(self, meta):
+    def __init__(self, fx_node=None):
         super().__init__()
-        self.meta = meta
+        self.fx_node = fx_node
+        self.operator = None
+
+    def __call__(self, operator=None, fx_node=None):
+        if operator:
+            self.operator = operator
+        if fx_node:
+            self.fx_node = fx_node
 
 
 class IRTensor(IRNode):
-    def __init__(self, meta, name: str = None, *args, **kwargs):
-        self.meta = meta
+    def __init__(self, name: str = None, *args, **kwargs):
         self.name = name
         self.args = args
         self.kwargs = kwargs
 
 
 class IRPointwise(IRNode):
-    def __init__(self, meta, operator=None, *args, **kwargs):
-        self.meta = meta
+    def __init__(self, operator=None, *args, **kwargs):
         self.operator = operator
         self.args = args
         self.kwargs = kwargs
 
 
 class IROperation(IRNode):
-    def __init__(self, meta, operator=None, *args, **kwargs):
-        self.meta = meta
+    def __init__(self, operator=None, *args, **kwargs):
+        self.operator = operator
+        self.args = args
+        self.kwargs = kwargs
+
+
+class IRReturn(IRNode):
+    def __init__(self, operator=None, *args, **kwargs):
         self.operator = operator
         self.args = args
         self.kwargs = kwargs
