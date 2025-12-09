88543a819cef3c09aa006e4e946130b9409cee87
jmaczan <jedrzejpawel@maczan.pl>
2025-12-09 23:17:55 +0100

    A skeleton of compiler passes

--- python/torch_webgpu/compiler/webgpu_compiler.py
+++ python/torch_webgpu/compiler/webgpu_compiler.py
@@ -1,22 +1,8 @@
 import torch
 from torch._dynamo import register_backend
 from typing import Callable, List
-from .ir import IRPointwise, IRTensor, IROperation, IRReturn
-
-fx_to_ir_dict = {
-    torch.tensor: IRTensor,
-    "add": IRPointwise,
-    torch.relu: IROperation,
-    "to": IROperation,
-    "output": IRReturn,
-}
-
-
-def get_ir(fx_operator):
-    ir_node = fx_to_ir_dict.get(fx_operator)
-    if ir_node:
-        ir_node(operator=fx_operator)
-    return ir_node
+from .ir import fx_to_ir
+from .compiler_pass import CompilerPass, Transform, Pattern
 
 
 @register_backend
@@ -26,30 +12,38 @@ def webgpu_backend(
     gm.graph.print_tabular()
     print(gm.graph)
 
-    ir_graph = []  #: List[IRNode]'
-    for i, node in enumerate(gm.graph.nodes):
-        ir_node = get_ir(node.target)
-        if ir_node:
-            ir_node(node)
-        else:
-            source_fn_stack = node.meta.get("source_fn_stack")
-            if source_fn_stack and len(source_fn_stack) > 0:
-                source_fn_stack = source_fn_stack[0]
-                if source_fn_stack and len(source_fn_stack) > 0:
-                    node_key = source_fn_stack[0]
-                    if node_key:
-                        ir_node = get_ir(node_key)
-        if ir_node:
-            ir_graph.append(ir_node(node.meta))
-        else:
-            print(f"Unsupported FX op: {node.target}. Current ir_graph: {ir_graph}")
-            raise Exception(
-                f"Unsupported FX op: {node.target}. Current ir_graph: {ir_graph}"
-            )
-        print(
-            f"#{i}: node:{node} node.target:{node.target}"  # Noqa E501
-        )  # Noqa E501
+    # OPTIMIZATION PASSES
+
+    ir_graph = fx_to_ir(gm)
+    ir_graph = optimize(ir_graph)
+
+    # LOWERING
+
+    # output_ir_graph
+
+    # BUILD A COMPILED FN (closure? lambda?) AND RETURN
 
     # TODO: see if it's still relevant
     # https://docs.pytorch.org/docs/stable/generated/torch.jit.optimize_for_inference.html
     return gm.forward
+
+
+def optimize(input_ir_graph):
+    passes = [
+        CompilerPass(
+            transforms=[
+                Transform(
+                    pattern=[
+                        Pattern("operator", "add"),
+                        Pattern("operator", "relu"),
+                    ],
+                    output="fused_add_relu",
+                )
+            ]
+        ),
+    ]
+    output_ir_graph = []
+    for p, compiler_pass in enumerate(passes):
+        output_ir_graph = compiler_pass.run(input_ir_graph)
+        input_ir_graph = output_ir_graph
+    return output_ir_graph
