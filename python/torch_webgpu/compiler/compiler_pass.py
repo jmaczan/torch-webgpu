72a7b507c429f0470f21541a17494881c99381a6
jmaczan <jedrzejpawel@maczan.pl>
2025-12-25 17:23:45 +0100

    Set value_id to fused ops, set inputs from ops that got fused into a new fused op and make sure no stale value_ids are inside inputs

--- python/torch_webgpu/compiler/compiler_pass.py
+++ python/torch_webgpu/compiler/compiler_pass.py
@@ -53,7 +53,7 @@ class CompilerPass(Generic[T_IRNode]):
             raise Exception(
                 "Compiler pass needs a dictionary that maps IR tops to IR nodes"
             )
-        output_graph = []
+        output_graph: list[T_IRNode] = []
         input_graph = ir_graph
         for t, transform in enumerate(self.transforms):
             output_graph = []
@@ -67,9 +67,20 @@ class CompilerPass(Generic[T_IRNode]):
                     and len(transform.pattern) > 0
                     and len(transform.pattern) + i - 1 < len(input_graph)
                 ):
+                    # The assumption here is that each element of pattern corresponds to one graph node
+                    # And they are in the same order as elements in graph
                     # the mechanism of matching needs to be more flexible;
-                    # a TODO for the future
+                    # to perhaps support more than a single pattern to check for a single node
+                    # and maybe be more flexible about the pattern ordering or something like that
+                    # In general, this is written mostly for fusion and needs to be generalized
+                    # For instance there could be value_id/inputs manipulation rules defined per transform
+                    # instead of one rule for all transforms
+                    # a big TODO for the future
                     is_pattern_match = True
+                    reuse_value_id_from_this_node: Optional[T_IRNode] = None
+                    inputs = []
+                    stale_value_ids = []
+                    num_patterns = len(transform.pattern)
                     for p, pattern in enumerate(transform.pattern):
                         if p == 0:
                             current_node_pattern_check = input_node
@@ -81,18 +92,44 @@ class CompilerPass(Generic[T_IRNode]):
                         if trait_value is None or trait_value != pattern.value:
                             is_pattern_match = False
                             break
+                        if p == num_patterns - 1:  # last pattern
+                            reuse_value_id_from_this_node = current_node_pattern_check
+                        else:
+                            inputs += current_node_pattern_check.inputs
+                            stale_value_ids += current_node_pattern_check.value_id
 
                     if is_pattern_match:
-                        if transform.output:
+                        inputs = list(dict.fromkeys(inputs))  # prevent duplicates
+                        stale_value_ids = list(
+                            dict.fromkeys(stale_value_ids)
+                        )  # value_ids that will be removed because of fusion
+
+                        inputs = [
+                            x for x in inputs if x not in set(stale_value_ids)
+                        ]  # remove value_ids that will not exist from inputs for fused node
+                        assert reuse_value_id_from_this_node is not None, (
+                            "Error scenario: since is_pattern_match is True, there had to be pattern matching and there has to be last node that was a part of a pattern"
+                        )
+                        if transform.output:  # TODO: maybe explicitly disallow empty outputs since they are not fully supported yet anyway
                             output_node = ir_op_to_ir_node.get(transform.output)  # Noqa E501
                             if not output_node:
                                 raise Exception("Trying to add empty node")
                             # the dirty part, to be refactored in the near never
                             if isinstance(input_node, HighIRNode):
-                                output_graph.append(output_node(input_node.fx_node))  # pyright: ignore[reportCallIssue]
+                                output_graph.append(
+                                    output_node(
+                                        fx_node=input_node.fx_node,  # pyright: ignore[reportCallIssue] TODO remove the ignore
+                                        value_id=reuse_value_id_from_this_node.value_id,
+                                        inputs=inputs,
+                                    )
+                                )
                             elif isinstance(input_node, LowIRNode):
                                 output_graph.append(
-                                    output_node(input_node.high_ir_node)  # pyright: ignore[reportCallIssue]
+                                    output_node(
+                                        high_ir_node=input_node.high_ir_node,  # pyright: ignore[reportCallIssue] TODO remove the ignore
+                                        value_id=reuse_value_id_from_this_node.value_id,
+                                        inputs=inputs,
+                                    )
                                 )
                             else:
                                 raise Exception(f"Unrecognized IR: {input_node}")
--- python/torch_webgpu/compiler/ir.py
+++ python/torch_webgpu/compiler/ir.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Any, List
 
 
 class IROp(ABC):
@@ -7,5 +8,13 @@ class IROp(ABC):
 
 
 class IRNode(ABC):
-    def __init__(self):
+    value_id: Any = None  # I assume that it's always defined for any concrete IRNode
+    inputs: List[Any] = []  # I assume that it can't be a None, but can be an empty list
+
+    def __init__(self, value_id: Any = None, inputs: List[Any] = []):
         super().__init__()
+
+        if value_id:
+            self.value_id = value_id
+        if inputs:
+            self.inputs = inputs
