6900c13e636a137d0ce1072658e10766a2af0178
jmaczan <jedrzejpawel@maczan.pl>
2026-01-01 14:24:45 +0100

    Extract mm shader into a separate .wgsl file, and finally I can have a sane syntax highlight for WGSL shaders

--- csrc/ops/arithmetic.cpp
+++ csrc/ops/arithmetic.cpp
@@ -7,6 +7,9 @@
 #include "core/webgpu_allocator.h"
 #include "binary.h"
 #include "utils/math.h"
+#include <fstream>
+#include <filesystem>
+#include <sstream>
 
 namespace torch_webgpu
 {
@@ -17,68 +20,6 @@ namespace torch_webgpu
             // TODO: no idea if optimal, just to start with something
             static constexpr uint32_t TILE_X = 16;
             static constexpr uint32_t TILE_Y = 16;
-
-            struct CacheHash
-            {
-                template <typename T>
-                std::size_t operator()(T t) const noexcept
-                {
-                    return static_cast<std::size_t>(t);
-                }
-            };
-
-            // TODO: improve performance, currently it's a native impl
-            // based on my first CUDA matrix multiplication kernel
-            const std::string mm_shader = R"wgsl(
-// A(M,N) x B(N,K) = C(M,K), where M,N,K are dims
-const MAX_DIMS: u32 = 8u;
-
-struct Params {
-    M: u32,
-    N: u32,
-    K: u32,
-    _pad: u32,
-
-    self_offset: u32,
-    mat2_offset: u32,
-    out_offset: u32,
-    _pad2: u32,
-
-    self_strides: array<u32, MAX_DIMS>,
-    mat2_strides: array<u32, MAX_DIMS>,
-    out_strides: array<u32, MAX_DIMS>,
-};
-
-@group(0) @binding(0)
-var<storage, read> A: array<f32>; // self
-
-@group(0) @binding(1)
-var<storage, read> B: array<f32>; // mat2
-
-@group(0) @binding(2)
-var<storage, read_write> C: array<f32>; // out
-
-@group(0) @binding(3)
-var<uniform> params: Params;
-
-// one thread = one output element C[column, row]
-@compute @workgroup_size(16, 16, 1)
-fn main(@builtin(global_invocation_id) gid: vec3<u32>) {
-    let col = gid.x; // 0..K-1
-    let row = gid.y; // 0..M-1
-    if (col >= params.K || row >= params.M) { return; }
-
-    var acc: f32 = 0.0;
-
-    for (var k: u32 = 0u; k < params.N; k = k + 1u) {
-        let a_idx = params.self_offset + row * params.N + k;
-        let b_idx = params.mat2_offset + k * params.K + col;
-        acc = acc + A[a_idx] * B[b_idx];
-    }
-    let c_idx = params.out_offset + row * params.K + col;
-    C[c_idx] = acc;
-}
-)wgsl";
         }
 
         void add_kernel_webgpu(::at::TensorIteratorBase &iter, const ::at::Scalar &alpha)
@@ -99,6 +40,16 @@ fn main(@builtin(global_invocation_id) gid: vec3<u32>) {
             TORCH_CHECK(self.scalar_type() == c10::ScalarType::Float);
             TORCH_CHECK(mat2.scalar_type() == c10::ScalarType::Float);
             TORCH_CHECK(out.scalar_type() == c10::ScalarType::Float);
+            static std::string mm_shader;
+            if (mm_shader.empty())
+            {
+                const std::filesystem::path shader_path = std::filesystem::path(__FILE__).parent_path().parent_path() / "shaders" / "mm.wgsl";
+                std::ifstream file(shader_path);
+                TORCH_CHECK(file.is_open(), "Failed to open mm shader");
+                std::stringstream ss;
+                ss << file.rdbuf();
+                mm_shader = ss.str();
+            }
 
             wgpu::ShaderSourceWGSL shader_source{
                 wgpu::ShaderSourceWGSL::Init{
