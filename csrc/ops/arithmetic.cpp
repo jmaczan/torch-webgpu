c0aa1e74c6f93f8e4176dab962ae76d46907c675
jmaczan <jedrzejpawel@maczan.pl>
2026-01-04 22:38:33 +0100

    Write matmul from scratch

--- csrc/ops/arithmetic.cpp
+++ csrc/ops/arithmetic.cpp
@@ -231,7 +231,7 @@ namespace torch_webgpu
             const uint32_t x_group_size = ceil_div_u32(params.K, TILE_X);
             const uint32_t y_group_size = ceil_div_u32(params.M, TILE_Y);
 
-            pass_encoder.DispatchWorkgroups(x_group_size, y_group_size, 1);
+            pass_encoder.DispatchWorkgroups(params.K * params.M);
             pass_encoder.End();
 
             wgpu::CommandBuffer command_buffer = encoder.Finish();
